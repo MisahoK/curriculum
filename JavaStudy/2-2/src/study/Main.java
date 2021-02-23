06fcc7f4163aa71ab2af7297044d2d266cc748a6
MisahoK <78588256+MisahoK@users.noreply.github.com>
2021-02-24 06:36:43 +0900

    mainメソッド・インスタンス・メソッドの呼び出し
    課題2-2の提出

---
+++ JavaStudy/2-2/src/study/Main.java
@@ -0,0 +1,17 @@
+package study;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        // ① 以下のルールに従いAccountクラスのインスタンスを生成してください。
+        // 変数名:「account」
+        Account account = new Account("hoge", 123, "abc123");
+
+        // ② ①のインスタンス変数より、アカウント情報をコンソール出力するメソッドを呼び出しなさい。
+        account.printAccountInfo();
+
+
+    }
+
+}
